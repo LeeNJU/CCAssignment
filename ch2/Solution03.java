2d0b2d10cbee3baa0f894da3d99d0466e33c5e6d
LeeNJU <lhb11@software.nju.edu.cn>
2015-09-10 18:05:00 -0400

    solution03 for chapter 2

---
+++ ch2/Solution03.java
@@ -0,0 +1,26 @@
+package Week1;
+
+class ListNode
+{
+	public int val;
+	public ListNode next;
+	public ListNode(int value)
+	{
+		this.val = value;
+	}
+}
+
+public class Solution03 {
+//assume that variable node is the middle node we are going to delete, swap the value of node
+	// and the value of node's successor. then link node and its successor's successor
+	public void deleteMiddleNode(ListNode node)
+	{
+		if(node == null)
+        	return ;
+        
+        int t = node.val;
+        node.val = node.next.val;
+        node.next.val = t;
+        node.next = node.next.next;   
+	}
+}
